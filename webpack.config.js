1a990d2e5eb85aa03b635792b1815265add817a9
kurone-kito <krone@kit.black>
2018-12-19 15:59:29 +0900

    Fixed the some code that according to TSLint rules

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const convPaths = require('convert-tsconfig-paths-to-webpack-aliases').default
+const convPaths = require('convert-tsconfig-paths-to-webpack-aliases').default;
 
 // Needs to be valid JSON. All comments in tsconfig.json must be removed.
-const tsconfig = require('./tsconfig.json')
+const tsconfig = require('./tsconfig.json');
 
-const aliases = convPaths(tsconfig)
+const aliases = convPaths(tsconfig);
 
 // Consumable std. Webpack Export
-module.exports = { resolve: { alias: aliases } }
+module.exports = { resolve: { alias: aliases } };
