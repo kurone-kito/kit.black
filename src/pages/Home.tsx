dbbc247eff1588259d186d92dd9bbb43825eb8df
kurone-kito <krone@kit.black>
2019-01-08 15:27:37 +0900

    Composited the molecule component and refactored the Home page component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,47 +1,21 @@
 import React from 'react';
 import { withSiteData } from 'react-static';
 
-import imgKito from '~/images/kito.png';
-import imgMarshmallow from '~/images/marshmallow.svg';
+import Logo from '~/atoms/Logo';
+import YouTube from '~/atoms/YouTube';
+import About from '~/molecules/About';
+import Nav from '~/molecules/Nav';
 
 const component: React.FC = () => (
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <a href="https://github.com/kurone-kito" target="_blank">
-            <span className="icon">
-              <i className="fab fa-github fa-3x" />
-            </span>
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/kurone_kito" target="_blank">
-            <span className="icon">
-              <i className="fab fa-twitter fa-3x" />
-            </span>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/channel/UCJs_ejHQM0rcemJaeO2s5vA" target="_blank">
-            <span className="icon">
-              <i className="fab fa-youtube fa-3x" />
-            </span>
-          </a>
-        </li>
-        <li>
-          <a href="https://marshmallow-qa.com/kurone_kito" target="_blank">
-            <img alt="" src={imgMarshmallow} />
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <header>
-      <h1 className="title is-1 kit-logo">KURONEKITO</h1>
-      <h2 className="subtitle">VTuber &amp; Web Programmer</h2>
-      <img alt="" src={imgKito} />
-    </header>
-  </div>
+  <React.Fragment>
+    <Nav />
+    <Logo />
+    <About />
+    <YouTube id="K1QRWeIDSdk" />
+    <footer className="footer">
+      <p>©︎2018-2019 Kurone Kito</p>
+    </footer>
+  </React.Fragment>
 );
 
 component.displayName = 'Home';
