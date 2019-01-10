35d1fd8b3857dafddbe73737e3751854f4e68adf
kurone-kito <krone@kit.black>
2019-01-11 08:50:26 +0900

    Improved the viewport type detection

--- src/stores/view.ts
+++ src/stores/view.ts
@@ -1,11 +1,10 @@
-import { Bulma } from 'bloomer/lib/bulma';
 import { action, observable } from 'mobx';
 
-import { viewport } from '~/utils/state';
+import { viewport, ViewportType } from '~/utils/state';
 
 export default class ViewStore {
   /** Type of current viewport width. */
-  @observable public viewportType: Bulma.Platform = viewport();
+  @observable public viewportType: ViewportType = viewport();
 
   /** Update the type of the viewport width. */
   @action public updateCurrentType() {
--- src/utils/state.ts
+++ src/utils/state.ts
@@ -1,26 +1,24 @@
-import { Bulma } from 'bloomer/lib/bulma';
 
-/** Viewport table. */
-interface IViewportTable {
-  /** Threshold of maximum. */
-  max: number;
-  /** Threshold of minimum. */
-  min: number;
-  /** type. */
-  type: Bulma.Platform;
+/** Viewport type. */
+export enum ViewportType {
+  /** Mobile screen. */
+  mobile = 'screen and (max-aspect-ratio: 4/5)',
+  /** Tablet size screen. */
+  tablet = 'screen and (max-aspect-ratio: 26/20)',
+  /** Desktop size screen. */
+  desktop = 'screen',
+  /** Others. */
+  others = 'all',
 }
 
 /** Viewport table. */
-const viewportTable: IViewportTable[] = [
-  { max: 769, min: Number.NEGATIVE_INFINITY, type: 'mobile' },
-  { max: 1024, min: 769, type: 'tablet' },
-  { max: 1216, min: 1024, type: 'desktop' },
-  { max: Number.POSITIVE_INFINITY, min: 1216, type: 'widescreen' },
+const viewportTable: ViewportType[] = [
+  ViewportType.mobile,
+  ViewportType.tablet,
+  ViewportType.desktop,
+  ViewportType.others,
 ];
 
 /** Detect current viewport type. */
-export const viewport = () => {
-  const { clientWidth } = document.documentElement;
-
-  return viewportTable.find(({ max, min }) => clientWidth >= min && clientWidth < max).type;
-};
+export const viewport = () =>
+  viewportTable.find(query => window.matchMedia(query).matches);
