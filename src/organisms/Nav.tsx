72ac9051cab01ebbfb79590028ba7be3b9cd63b5
kurone-kito <krone@kit.black>
2019-01-29 14:58:27 +0900

    Implemented that by tapping the outside of NavBar, it'll be collapsing

--- src/organisms/Nav.tsx
+++ src/organisms/Nav.tsx
@@ -1,16 +1,38 @@
 import { Navbar } from 'bloomer';
+import { inject, observer } from 'mobx-react';
 import React from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 import NavBrand from '~/atoms/NavBrand';
 import NavMenu from '~/molecules/NavMenu';
+import NavStore from '~/stores/nav';
 
-const component: React.FC = () => (
-  <Navbar>
-    <NavBrand />
-    <NavMenu />
-  </Navbar>
-);
+/** Type of properties. */
+interface IProps {
+  /** Store for navigation menu. */
+  navStore: NavStore;
+}
 
-component.displayName = 'Nav';
+@inject(({ navStore }): IProps => ({ navStore }))
+@observer
+export default class Nav extends React.Component<IProps> {
+  public render() {
+    return (
+      <OutsideClickHandler onOutsideClick={this.onOutsideClick}>
+        <Navbar>
+          <NavBrand />
+          <NavMenu />
+        </Navbar>
+      </OutsideClickHandler>
+    );
+  }
 
-export default component;
+  /** Called on tapping outside of component. */
+  private onOutsideClick = () => {
+    const { navStore: { expanded, toggleExpantion } } = this.props;
+
+    if (expanded) {
+      toggleExpantion();
+    }
+  }
+}
