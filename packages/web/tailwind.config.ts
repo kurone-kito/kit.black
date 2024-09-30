5964405fd916836b48d7ccb2663c2d4fba594b3f
kurone-kito <krone@kit.black>
2024-09-30 21:15:23 +0900

    feat(root,web): customized the theme with the daisy-ui

--- packages/web/tailwind.config.ts
+++ packages/web/tailwind.config.ts
@@ -1,5 +1,27 @@
+import daisyUi from 'daisyui';
 import type { Config } from 'tailwindcss';
+
+import containerQueries = require('@tailwindcss/container-queries');
+import safeArea = require('tailwindcss-safe-area');
 
 export default {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  plugins: [daisyUi, containerQueries, safeArea],
+  theme: {
+    extend: {
+      fontFamily: {
+        Lato: ['Lato'],
+        sans: [
+          'Helvetica Neue',
+          'Arial',
+          'Hiragino Sans',
+          'BIZ UDPGothic',
+          'Yu Gothic',
+          'YuGothic',
+          'Meiryo',
+          'sans-serif',
+        ],
+      },
+    },
+  },
 } satisfies Config;
