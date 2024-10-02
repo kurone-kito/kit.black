4e85e73b50fe7905d0233a5446ba106329cef032
kurone-kito <krone@kit.black>
2024-10-03 01:16:58 +0900

    feat(web): added the atomic component that the placeholder of the avatar

---
+++ packages/web/src/components/atoms/Avatar.tsx
@@ -0,0 +1,45 @@
+import type { Component, JSX } from 'solid-js';
+import { twMerge } from 'tailwind-merge';
+
+/** Type definition for the properties. */
+export interface AvatarProps
+  extends Pick<
+    Readonly<JSX.ImgHTMLAttributes<HTMLImageElement>>,
+    'alt' | 'class' | 'height' | 'id' | 'src' | 'width'
+  > {
+  /** The CSS classes for anchor. */
+  readonly anchorClass?: string | undefined;
+
+  /** The ID of the next element. */
+  readonly nextId: string;
+}
+
+/**
+ * The avatar component.
+ * @param props The properties.
+ * @returns The component.
+ */
+export const Avatar: Component<AvatarProps> = (props) => (
+  <div
+    id={props.id}
+    class={twMerge('aspect-[46/89] h-auto w-full', props.class)}
+  >
+    <img
+      alt={props.alt}
+      class="w-full object-contain drop-shadow-lg"
+      height={props.height}
+      src={props.src}
+      width={props.width}
+    />
+    <a
+      href={`#${props.nextId}`}
+      class={twMerge(
+        'absolute left-0 right-0 mx-auto cursor-grab max-md:hidden',
+        props.anchorClass,
+      )}
+      role="none"
+    >
+      &nbsp;
+    </a>
+  </div>
+);
