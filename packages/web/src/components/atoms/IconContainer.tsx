1c17d1992f81e015c3ec1e56c3be9eb2d9377ae6
kurone-kito <krone@kit.black>
2024-10-03 13:34:43 +0900

    feat(web): added the atomic component that the placeholder of the icon

---
+++ packages/web/src/components/atoms/IconContainer.tsx
@@ -0,0 +1,24 @@
+import type { Component, ParentProps } from 'solid-js';
+import { mergeProps } from 'solid-js/web';
+import { twMerge } from 'tailwind-merge';
+
+/** Type definition for the properties. */
+export interface IconProps extends Readonly<ParentProps> {
+  /** The CSS class. */
+  readonly class?: string | undefined;
+}
+
+/** The default properties. */
+const defaultProps = { children: '◆' } as const satisfies IconProps;
+
+/**
+ * The icon component.
+ * @param props The properties.
+ * @returns The component.
+ */
+export const IconContainer: Component<IconProps> = (props) => {
+  const concProps = mergeProps(defaultProps, props);
+  return (
+    <i class={twMerge('not-italic', concProps.class)}>{concProps.children}</i>
+  );
+};
