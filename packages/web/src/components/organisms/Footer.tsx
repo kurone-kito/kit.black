fb02104f357f59e739a1421569f88a3f3f00644a
kurone-kito <krone@kit.black>
2024-10-10 10:11:23 +0900

    feat(web): improved the organism component that the footer

--- packages/web/src/components/organisms/Footer.tsx
+++ packages/web/src/components/organisms/Footer.tsx
@@ -1,6 +1,9 @@
 import type { Component } from 'solid-js';
+import { FaBrandsGithub } from 'solid-icons/fa';
+import { SiNetlify } from 'solid-icons/si';
 import constants from '../../constants.json';
 import { now } from '../../modules/datetime.js';
+import { FooterItem } from '../molecules/FooterItem.js';
 
 /**
  * The footer component.
@@ -8,10 +11,21 @@ import { now } from '../../modules/datetime.js';
  */
 export const Footer: Component = () => (
   <footer class="footer footer-center bg-base-300 text-base-content px-safe pb-safe-or-6 pt-6 font-extralight">
-    <aside class="font-thin tracking-wide" translate="no">
+    <aside class="flex font-thin tracking-wide" translate="no">
       <p>
         &copy; 2018-{now().getFullYear()} {constants.author.name}
       </p>
+      <ul class="menu menu-horizontal">
+        <FooterItem
+          href="https://github.com/kurone-kito/kit.black"
+          tip="Repository"
+        >
+          <FaBrandsGithub class="inline" />
+        </FooterItem>
+        <FooterItem href="https://www.netlify.com" tip="Powered by Netlify">
+          <SiNetlify class="inline" />
+        </FooterItem>
+      </ul>
     </aside>
   </footer>
 );
