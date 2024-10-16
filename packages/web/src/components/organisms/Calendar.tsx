eb8c9c10cb86d8a1c076546a4f5fe6844cda5f3b
kurone-kito <krone@kit.black>
2024-10-16 17:08:01 +0900

    feat(web): improved the organism component that the calendar

--- packages/web/src/components/organisms/Calendar.tsx
+++ packages/web/src/components/organisms/Calendar.tsx
@@ -1,10 +1,10 @@
 import { tupleMap, weekRange, formatDate } from '@kurone-kito/kit.black-lib';
 import type { Component } from 'solid-js';
 import rows from '../../data.json';
+import { now } from '../../modules/datetime.js';
 import { Article } from '../atoms/Article.js';
 import type { RowProps } from '../atoms/calendar/Row.js';
 import { Calendar as MoleculeCalendar } from '../molecules/calendar/Calendar.js';
-import { now } from '../../modules/datetime.js';
 
 const [since, until] = tupleMap(weekRange(now()), formatDate);
 
@@ -17,6 +17,10 @@ export const Calendar: Component = () => (
     class="!px-safe flex flex-col justify-center xl:w-10/12"
     heading="#VTuber予定表"
   >
+    <p>
+      以下の配信スケジュールは毎日 8 回、カレンダー
+      アプリより抽出したスケジュールに基づき、自動生成しております。
+    </p>
     <MoleculeCalendar
       id="calendar"
       rows={rows as readonly RowProps[]}
