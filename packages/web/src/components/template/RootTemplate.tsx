54cf24358c1a9ee0796cd40f9ea46ec763dbe1df
kurone-kito <krone@kit.black>
2024-10-16 22:23:43 +0900

    feat(root,web): added the organism component that the navbar

--- packages/web/src/components/template/RootTemplate.tsx
+++ packages/web/src/components/template/RootTemplate.tsx
@@ -1,21 +1,27 @@
 import { MetaProvider } from '@solidjs/meta';
 import type { RouteSectionProps } from '@solidjs/router';
 import type { Component } from 'solid-js';
-import { Suspense } from 'solid-js';
+import { onMount, Suspense } from 'solid-js';
+import { themeChange } from 'theme-change';
 import { Footer } from '../organisms/Footer.js';
 import { Head } from '../organisms/Head.js';
+import { Navbar } from '../organisms/Navbar.js';
 
 /**
  * The root template component.
  * @param props The properties.
  * @returns The component.
  */
-export const RootTemplate: Component<RouteSectionProps> = (props) => (
-  <MetaProvider>
-    <Head />
-    <main>
-      <Suspense fallback={<div>Loading...</div>}>{props.children}</Suspense>
-    </main>
-    <Footer />
-  </MetaProvider>
-);
+export const RootTemplate: Component<RouteSectionProps> = (props) => {
+  onMount(() => themeChange(false));
+  return (
+    <MetaProvider>
+      <Head />
+      <Navbar />
+      <main>
+        <Suspense fallback={<div>Loading...</div>}>{props.children}</Suspense>
+      </main>
+      <Footer />
+    </MetaProvider>
+  );
+};
