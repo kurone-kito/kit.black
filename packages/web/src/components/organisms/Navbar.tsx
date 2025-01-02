91fe589a3dbe1a0a52399d35e71416f583a565ed
kurone-kito <krone@kit.black>
2025-01-03 01:50:35 +0900

    feat(web): improved the organisms components

--- packages/web/src/components/organisms/Navbar.tsx
+++ packages/web/src/components/organisms/Navbar.tsx
@@ -1,8 +1,8 @@
 import type { Component } from 'solid-js';
 import { createSignal } from 'solid-js';
 import { HamburgerButton } from '../atoms/HamburgerButton.js';
-import { ToggleTheme } from './ToggleTheme.js';
 import { LanguageChanger } from './LanguageChanger.js';
+import { ToggleTheme } from './ToggleTheme.js';
 
 /**
  * The navigation bar.
@@ -11,10 +11,16 @@ import { LanguageChanger } from './LanguageChanger.js';
 export const Navbar: Component = () => {
   const [expanded, setExpandState] = createSignal(false);
   return (
-    <header class="navbar pointer-events-none fixed top-0 z-50 justify-end">
+    <header
+      aria-label="Main navigation"
+      class="navbar pointer-events-none fixed top-0 z-50 justify-end"
+      role="navigation"
+    >
       <ul
+        aria-expanded={expanded()}
         class="navbar-end max-lg:bg-primary/90 bg-primary/40 hover:bg-primary/90 pointer-events-auto flex items-center justify-between rounded-lg transition-all"
         classList={{ 'w-14': !expanded(), 'w-44': expanded() }}
+        role="menubar"
       >
         <li classList={{ hidden: !expanded() }}>
           <LanguageChanger />
