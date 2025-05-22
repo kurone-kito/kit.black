a7a4685ab87c09c4e7a54b11a336855ff1bcb917
kurone-kito <krone@kit.black>
2025-05-22 12:42:21 +0900

    feat(web): removed the splash screen

--- packages/web/src/components/organisms/Head.tsx
+++ packages/web/src/components/organisms/Head.tsx
@@ -1,6 +1,6 @@
 import type { Component } from 'solid-js';
 import constants from '../../constants.json';
-import { useTranslator } from '../../modules/createI18N.js';
+import { createI18N, useLanguage } from '../../modules/createI18N.js';
 import { Head as InternalHead } from '../molecules/Head.js';
 
 /** The images for the Open Graph protocol. */
@@ -11,7 +11,8 @@ const images = [constants.favicon.url] as const;
  * @returns The component.
  */
 export const Head: Component = () => {
-  const t = useTranslator();
+  const language = useLanguage();
+  const t = createI18N(language);
   return (
     <InternalHead
       author={t('author')}
@@ -27,7 +28,7 @@ export const Head: Component = () => {
       images={images}
       imageType={constants.favicon.type}
       imageWidth={constants.favicon.size}
-      language="ja"
+      language={language()}
       licenseUrl={constants.licenseUrl}
       next={undefined}
       prev={undefined}
--- packages/web/src/components/template/RootTemplate.tsx
+++ packages/web/src/components/template/RootTemplate.tsx
@@ -1,13 +1,11 @@
-import { createMediaQuery } from '@solid-primitives/media';
 import { MetaProvider } from '@solidjs/meta';
 import type { RouteSectionProps } from '@solidjs/router';
 import type { Component } from 'solid-js';
-import { Show, Suspense, createSignal, onMount } from 'solid-js';
+import { Suspense, onMount } from 'solid-js';
 import { themeChange } from 'theme-change';
 import { Footer } from '../organisms/Footer.js';
 import { Head } from '../organisms/Head.js';
 import { Navbar } from '../organisms/Navbar.js';
-import { DURATION, Splash } from '../organisms/Splash.js';
 
 /**
  * The root template component.
@@ -15,19 +13,11 @@ import { DURATION, Splash } from '../organisms/Splash.js';
  * @returns The component.
  */
 export const RootTemplate: Component<RouteSectionProps> = (props) => {
-  const reduceMotion = createMediaQuery('(prefers-reduced-motion: reduce)');
-  const [isSplash, setSplash] = createSignal<boolean>(!reduceMotion());
-  onMount(() => {
-    themeChange(false);
-    setTimeout(() => setSplash(false), DURATION);
-  });
+  onMount(() => themeChange(false));
   return (
     <MetaProvider>
       <Head />
       <Navbar />
-      <Show when={isSplash()}>
-        <Splash />
-      </Show>
       <Suspense>
         <main>
           {props.children}
